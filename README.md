61c619b65ddf4b3bc81a52d190e8558a875ac9db
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-07-08 12:33:31 +0200

    Added version 1.0.1

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-compatibility
-=============
+Compatibility plugin for Craft CMS
+=================
 
-Craft Compatibility Plugin
+Plugin that makes Craft's CMS work in older browsers
+
+Features:
+ - Adds a shim for HTML5 History API
+ - Fixes CSS for older browsers
+ 
+Todo:
+ - Import jQuery 1x over the current 2x
+
+
+Changelog
+=================
+###1.0.1###
+ - Fixed the path to the resource url's
+ 
+###1.0###
+ - Initial release
---
+++ compatibility/CompatibilityPlugin.php
@@ -0,0 +1,45 @@
+<?php
+namespace Craft;
+
+class CompatibilityPlugin extends BasePlugin
+{
+	function getName()
+	{
+		return Craft::t('Browser Compatibility');
+	}
+
+	function getVersion()
+	{
+		return '1.0.1';
+	}
+
+	function getDeveloper()
+	{
+		return 'Bob Olde Hampsink';
+	}
+
+	function getDeveloperUrl()
+	{
+		return 'http://www.itmundi.nl';
+	}
+	
+	function init() {
+	
+		if(craft()->request->isCpRequest()) {
+						
+			// Check if IE7 - 9
+			if(preg_match('/(?i)msie [7-9]/',$_SERVER['HTTP_USER_AGENT'])) {
+			
+			    // Extend History API
+			    craft()->templates->includeFootHtml('<script type="text/javascript" src="'.UrlHelper::getResourceUrl('compatibility/js/native.history.js').'"></script>');
+			    craft()->templates->includeFootHtml('<script type="text/javascript">var history = History;</script>');
+			
+			    // CSS fixes
+			    craft()->templates->includeCssResource('compatibility/css/style.css');
+			    
+		    }
+			
+		}
+		
+	}
+}
